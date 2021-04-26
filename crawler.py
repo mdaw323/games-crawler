2636235c53f663c462984310efdb816e86fb629f
mdaw <45016616+mdaw323@users.noreply.github.com>
2021-04-26 16:38:58 +0200

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,59 @@
+import os.path
+import re
+import requests
+import time
+from fake_useragent import UserAgent
+
+headers = {'User-Agent': UserAgent().firefox}
+data_directory = 'data'
+
+
+def read_main_page():
+    resp = requests.get("https://theweekinchess.com/twic", headers=headers)
+    if resp.status_code == 200:
+        return resp.text
+
+
+def read_response_from_file():
+    with open("twic.html", 'r') as f:
+        return f.read()
+
+
+def data_path(filename: str):
+    return data_directory + '/' + filename
+
+
+def file_exists(filename: str):
+    return os.path.isfile(data_path(filename))
+
+
+def main():
+    f = read_main_page()
+    sites = [(site.split('/')[-1], site)
+             for site in re.findall(r'https.*g.zip', f)]
+    sites_to_download = []
+    print(f'Total weeks found: {len(sites)}, '
+          'missing weeks to download: {len(sites_to_download)}')
+
+    for filename, site in sites:
+        if not file_exists(filename):
+            sites_to_download.append((filename, site))
+
+    for idx, (filename, site) in enumerate(sites_to_download):
+        print(f'wait 3 seconds before download')
+        time.sleep(3)
+        print(f'download [{idx+1}/{len(sites_to_download)}] {site} '
+              'save to {filename}')
+        resp = requests.get(site, headers=headers)
+        if resp.status_code == 200:
+            if not os.path.isfile(filename):
+                with open(data_path(filename), 'wb') as f:
+                    f.write(resp.content)
+            else:
+                print(f'file conflict: {site}, {filename}')
+        else:
+            print(f'status: {resp.status_code}')
+
+
+if __name__ == '__main__':
+    main()
